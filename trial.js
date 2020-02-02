b47d94406cdd909d768239492dc8ec8b2069a46e
unknown <bagus.wisanggeni4897@gmail.com>
2020-02-02 12:14:01 +0700

    editing few code

--- trial.js
+++ trial.js
@@ -1,10 +1,10 @@
-const game = new Phaser.Game(1280, 720, Phaser.AUTO, '',
+const game = new Phaser.Game(1024, 768, Phaser.AUTO, '',
 { preload: preload, create: create ,update: update });
 
-let platforms
-let platforms2
+var platforms
+var platforms2
 var player
-let fin
+var fin
 var kompor
 
 function preload() {
@@ -17,6 +17,7 @@ function preload() {
   game.load.image('api','assets/stage7/api.png')
   game.load.image('mentega','assets/stage7/mentega.png')
   game.load.image('mentega1','assets/stage7/mentega1.png')
+  game.load.physics('sprite_physics','assets/stage7/p2_physics.json')
 
 
   //inserting asset position of finish
@@ -25,30 +26,59 @@ function preload() {
 
 
 function create() {
+  game.add.sprite(0,0,'bg')
   //add game physics
-    game.physics.startSystem(Phaser.Physics.ARCADE)
+    game.physics.startSystem(Phaser.Physics.P2JS)
+    var platformsCollison = game.physics.p2.createCollisionGroup()
+    game.physics.p2.updateBoundsCollisionGroup()
 
+    platforms = game.add.group()
+    platforms.enableBody = true
+    game.physics.p2.gravity.y = 8600;
+
+    
     //add background
-    game.add.sprite(0,0,'bg')
-    kompor = game.add.sprite(0,650,'kompor')
-    game.physics.arcade.enable(kompor)
-    kompor.enableBody = true
+    
+
+     mentega = platforms.create(900,350,'mentega')
+    //make mentega body can't be moved
+    mentega.body.immovable = true
+  
+     mentega1 = platforms.create(810,440,'mentega1')
+    mentega1.body.immovable = true
+  
+     kompor = platforms.create(0,555,'kompor')
     kompor.body.immovable = true
+  
+     kompor1 = platforms.create(215,500,'kompor1')
+    kompor1.body.immovable = true
+  
+     kompor2 = platforms.create(150,525,'kompor2')
+    kompor2.body.immovable = true
+    // mentega = game.add.sprite(100,100,'mentega')
+    // kompor = game.add.sprite(0,500,'kompor')
+
+   
+    // // game.physics.arcade.enable(kompor)
+    // // kompor.enableBody = true
+    // // kompor.body.immovable = true
 
 
-    fin = game.add.sprite(1015,320,'finish')
-    game.physics.arcade.enable(fin)
-    fin.enableBody = true
-    fin.body.immovable = true
+    
+    // // game.physics.arcade.enable(fin)
+    // // fin.enableBody = true
+    // // fin.body.immovable = true
 
 
-    player = game.add.sprite(0,500,'char')
-    game.physics.arcade.enable(player)
-    player.body.bounce.y = 0.1
-    player.body.gravity.y = 800
-    player.body.collideWorldBounds = true
+    player = game.add.sprite(100,500,'char')
+    game.physics.p2.enable(player)
+    player.body.setZeroDamping();
+	  player.body.fixedRotation = true
+    // player.body.bounce.y = 0.1
+    // player.body.gravity.y = 800
+    // player.body.collideWorldBounds = true
 
-    cursors = game.input.keyboard.createCursorKeys()
+    cursors = game.input.keyboard.createCursorKeys();
 
 
 
@@ -56,24 +86,32 @@ function create() {
 }
 
 function update() {
-game.physics.arcade.collide(player, platforms)
-game.physics.arcade.collide(player, kompor)
+      // game.physics.arcade.collide(player, platforms)
+      // game.physics.arcade.collide(player, kompor)
 
-// game.physics.arcade.collide(player, platforms3, reload, null, this)
-// game.physics.arcade.collide(player, spon, drop, null,this)
-game.physics.arcade.collide(player, fin, nextStage, null, this);
+      // game.physics.arcade.collide(player, platforms3, reload, null, this)
+      // game.physics.arcade.collide(player, spon, drop, null,this)
+//      game.physics.arcade.collide(player, fin, nextStage, null, this);
 
-player.body.velocity.x = 1
+      // player.body.velocity.x = 1
+      player.body.setZeroVelocity();
+      if (cursors.left.isDown)
+      {
+        player.body.moveLeft(400);
+      }
+      else if (cursors.right.isDown)
+      {
+        player.body.moveRight(400);
+      }
 
-if(cursors.left.isDown){
-  player.body.velocity.x = -150
-} else if (cursors.right.isDown){
-  player.body.velocity.x = 150
-}
-
-if(cursors.up.isDown && player.body.touching.down){
-  player.body.velocity.y = -420
-}
+      if (cursors.up.isDown)
+      {
+        player.body.moveUp(400);
+      }
+      else if (cursors.down.isDown)
+      {
+        player.body.moveDown(400);
+      }
 
 
 }
